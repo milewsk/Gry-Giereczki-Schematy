3b707cb639c719f3228149e183901e05a6620d90
Konrad <konradmilewski2000@interia.eu>
2022-01-12 13:24:09 +0100

    Dodanie panelu logowania

--- js-react/src/components/Layout/MainContent.js
+++ js-react/src/components/Layout/MainContent.js
@@ -1,7 +1,16 @@
+import React, { useContext } from "react/cjs/react.development";
+import AuthContext from "../../store/AuthContext";
+import Login from "../Login/Login";
 import classes from "./MainContent.module.css";
 
 const MainContent = (props) => {
-  return <main className={classes.main_content}></main>;
+  const authCtx = useContext(AuthContext);
+  return (
+    <main className={classes.main_content}>
+      {!authCtx.isLoggedIn && <Login></Login>}
+      {authCtx.isLoggedIn && <div></div>}
+    </main>
+  );
 };
 
 export default MainContent;
