f789804b2189d317139dc8b42b231ea10da96c03
Rafał Rutkowski <65781331+shogieasy@users.noreply.github.com>
2022-03-07 21:07:09 +0100

    add registen be

--- GryGiereczki/Migrations/20220307182641_add_password.Designer.cs
+++ GryGiereczki/Migrations/20220307182641_add_password.Designer.cs
@@ -4,14 +4,16 @@ using GryGiereczki.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GryGiereczki.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220307182641_add_password")]
+    partial class add_password
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GryGiereczki/Models/User.cs
+++ GryGiereczki/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,11 +12,26 @@ namespace GryGiereczki.Models
         public int Id { get; set; }
         [Required, MaxLength(20)]
         public string Nick { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+        [Display(Name = "Password")]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Passwords must be at least 8 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The passwords do not match.")]
+        [NotMapped]
+        public string ConfirmPassword { get; set; }
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Display(Name = "Email")]
+        [Required, MaxLength(20)]
         public string Email { get; set; }
         [Required, MaxLength(30)]
+        [Display(Name = "First Name")]
         public string Name { get; set; }
         [Required, MaxLength(20)]
+        [Display(Name = "Last Name")]
         public string Lastname { get; set; }
         [Required]
         public string DateOfBirth { get; set; }
